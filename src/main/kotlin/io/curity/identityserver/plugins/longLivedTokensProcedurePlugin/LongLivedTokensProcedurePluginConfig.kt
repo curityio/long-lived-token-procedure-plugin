0244ff0007c7ed60dc73dc6dc11fd9eba04e899a
Michal Trojanowski <michal@curity.io>
2023-05-10 12:11:30 +0200

    Updated SDK to 8.2.0. Token issuers are now SDK services

--- src/main/kotlin/io/curity/identityserver/plugins/longLivedTokensProcedurePlugin/LongLivedTokensProcedurePluginConfig.kt
+++ src/main/kotlin/io/curity/identityserver/plugins/longLivedTokensProcedurePlugin/LongLivedTokensProcedurePluginConfig.kt
@@ -17,11 +17,27 @@ package io.curity.identityserver.plugins.longLivedTokensProcedurePlugin
 
 import se.curity.identityserver.sdk.config.Configuration
 import se.curity.identityserver.sdk.config.annotation.DefaultLong
+import se.curity.identityserver.sdk.config.annotation.DefaultService
 import se.curity.identityserver.sdk.config.annotation.Description
+import se.curity.identityserver.sdk.service.issuer.AccessTokenIssuer
+import se.curity.identityserver.sdk.service.issuer.IdTokenIssuer
+import se.curity.identityserver.sdk.service.issuer.RefreshTokenIssuer
 
 interface LongLivedTokensProcedurePluginConfig: Configuration
 {
     @Description("The Time To Live for a long-lived access token, in seconds. Defaults to 4 hours.")
     @DefaultLong(4 * 60 * 60)
     fun getLongLivedAccessTokenExpiration(): Long
+
+    @DefaultService
+    @Description("A custom access token issuer to be used in the procedure. The default issuer is used if none provided.")
+    fun getAccessTokenIssuer(): AccessTokenIssuer
+
+    @DefaultService
+    @Description("A custom refresh token issuer to be used in the procedure. The default issuer is used if none provided.")
+    fun getRefreshTokenIssuer(): RefreshTokenIssuer
+
+    @DefaultService
+    @Description("A custom ID token issuer to be used in the procedure. The default issuer is used if none provided.")
+    fun getIdTokenIssuer(): IdTokenIssuer
 }
